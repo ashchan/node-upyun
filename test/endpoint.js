3cd68a34bef57a08047712431cadb422f3d1ab9e
James Chen <ashchan@gmail.com>
2012-01-11 10:49:48 +0900

    expected should be after actual in t.equal

--- test/endpoint.js
+++ test/endpoint.js
@@ -3,20 +3,20 @@ var tap = require("tap")
   , endpoint = require("../lib/endpoint");
 
 test("available endpoints", function(t) {
-  t.equal("http://v0.api.upyun.com", endpoint.ENDPOINT_V0);
-  t.equal("http://v1.api.upyun.com", endpoint.ENDPOINT_V1);
-  t.equal("http://v2.api.upyun.com", endpoint.ENDPOINT_V2);
-  t.equal("http://v3.api.upyun.com", endpoint.ENDPOINT_V3);
+  t.equal(endpoint.ENDPOINT_V0, "http://v0.api.upyun.com");
+  t.equal(endpoint.ENDPOINT_V1, "http://v1.api.upyun.com");
+  t.equal(endpoint.ENDPOINT_V2, "http://v2.api.upyun.com");
+  t.equal(endpoint.ENDPOINT_V3, "http://v3.api.upyun.com");
   t.end();
 });
 
 test("default endpoint", function(t) {
-  t.equal("http://v0.api.upyun.com", endpoint.getEndpoint());
+  t.equal(endpoint.getEndpoint(), "http://v0.api.upyun.com");
   t.end();
 });
 
 test("set endpoint", function(t) {
   endpoint.setEndpoint(endpoint.ENDPOINT_V2);
-  t.equal("http://v2.api.upyun.com", endpoint.getEndpoint());
+  t.equal(endpoint.getEndpoint(), "http://v2.api.upyun.com");
   t.end();
 });
